c72085b21712e3063305b1d8d46de3b4a80ebde8
kbalamurugankbm01 <115966525+kbalamurugankbm01@users.noreply.github.com>
2022-11-28 21:38:01 +0530

    Update tshirts_test.c

--- tshirts_test.c
+++ tshirts_test.c
@@ -7,11 +7,12 @@ int main() {
     assert(size(40) == 'M');
     assert(size(43) == 'L');
 
-    // assert(size(42) == 'M'); //Fails
-    // assert(size(38) == 'S'); //Fails
-    // assert(size(-1) != 'L'); //Fails
-    // assert(size(INT_MAX) != 'L'); //Fails
-    // assert(size(INT_MIN) != 'L'); //Fails
+    assert(size(42) == 'M'); //Fails
+    assert(size(38) == 'S'); //Fails
+    
+    assert(size(-1) != 'L'); //Fails
+    assert(size(INT_MAX) != 'L'); //Fails
+    assert(size(INT_MIN) != 'L'); //Fails
 
     printf("All is well (maybe!)\n");
     return 0;
