231dfc5c6684e18f3794aff96c6680466274f68d
kbalamurugankbm01 <115966525+kbalamurugankbm01@users.noreply.github.com>
2022-12-06 18:05:23 +0530

    misaligned code fix

--- misaligned.c
+++ misaligned.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 #include "misaligned.h"
 
-int printColorMap() {
+ColorPair colorPairList[MAX_COLOR_PAIR_COUNT];
+
+void printColorMap() {
+    int size, i;
+    size = fillColorMapList();
+
+    for (i = 0; i < size; i++) {
+        printf("%d | %s | %s\n", i, colorPairList[i].majorColor, colorPairList[i].minorColor);
+    }
+}
+
+int fillColorMapList() {
     const char* majorColor[] = {"White", "Red", "Black", "Yellow", "Violet"};
     const char* minorColor[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
     int i = 0, j = 0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            printf("%d | %s | %s\n", i * 5 + j, majorColor[i], minorColor[i]);
+            strcpy(colorPairList[(i*5)+j].majorColor, majorColor[i]);
+            strcpy(colorPairList[(i*5)+j].minorColor, minorColor[j]);
         }
     }
     return i * j;
--- misaligned.h
+++ misaligned.h
@@ -1,6 +1,17 @@
 #ifndef MISALIGNED_H
 #define MISALIGNED_H
 
-extern int printColorMap();
+#define MAX_COLOR_PAIR_COUNT 25
+#define MAX_COLOR_NAME_LEN 10
+
+typedef struct {
+    char majorColor[MAX_COLOR_NAME_LEN];
+    char minorColor[MAX_COLOR_NAME_LEN];
+}ColorPair;
+
+extern void printColorMap();
+int fillColorMapList();
+
+extern ColorPair colorPairList[MAX_COLOR_PAIR_COUNT];
 
 #endif /*MISALIGNED_H*/
