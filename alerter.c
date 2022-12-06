b133f4da08879e119013c74eb91d23f0d53771f4
kbalamurugankbm01 <115966525+kbalamurugankbm01@users.noreply.github.com>
2022-12-06 21:39:09 +0530

    alerter code

--- alerter.c
+++ alerter.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "alerter.h"
-
+ 
 int alertFailureCount = 0;
 
 int networkAlertStub(float celcius) {
@@ -11,14 +11,26 @@ int networkAlertStub(float celcius) {
     return 200;
 }
 
-void alertInCelcius(float farenheit) {
+int networkAlert(float celcius) {
+    printf("ALERT: Temperature is %.1f celcius.\n", celcius);
+    // Return 200 for ok
+    // Return 500 for not-ok
+    if (celcius < 200.0) {
+        return 200;
+    }else {
+        return 500;
+    }
+
+}
+
+void alertInCelcius(networkAlert_ptr_t networkAlert_func, float farenheit) {
     float celcius = (farenheit - 32) * 5 / 9;
-    int returnCode = networkAlertStub(celcius);
+    int returnCode = networkAlert_func(celcius);
     if (returnCode != 200) {
         // non-ok response is not an error! Issues happen in life!
         // let us keep a count of failures to report
         // However, this code doesn't count failures!
         // Add a test below to catch this bug. Alter the stub above, if needed.
-        alertFailureCount += 0;
+        alertFailureCount += 1;
     }
 }
--- alerter.h
+++ alerter.h
@@ -1,8 +1,11 @@
 #ifndef ALERTER_H
 #define ALERTER_H
 
+typedef int (*networkAlert_ptr_t)(float);
+
 extern int alertFailureCount;
+extern int networkAlert(float celcius);
 extern int networkAlertStub(float celcius);
-extern void alertInCelcius(float farenheit);
+extern void alertInCelcius(networkAlert_ptr_t networkAlert_func, float farenheit);
 
 #endif /*ALERTER_H*/
